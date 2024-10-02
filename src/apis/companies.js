26171e6e53e1ce3ac00d786196b9c120523bf734
woonie_potato <backendwoonie@gmail.com>
2024-10-02 13:24:21 +0900

    feat: I'm doing the PR again since I didnt pull the latest files before I pushed.

--- src/apis/companies.js
+++ src/apis/companies.js
@@ -4,7 +4,42 @@ import { asyncHandler } from "../../utils/async-handler.js";
 
 export const companiesRouter = express.Router();
 
-// 기업 상세 조회 api
+// 기업 전체리스트 (메인페이지) API
+function getSortOption(query) {
+  const sortOption = {};
+
+  switch (query) {
+    case "totalInvestment":
+      sortOption.totalInvestment = "desc";
+      sortOption.revenue = "asc";
+      sortOption.employees = "asc";
+      break;
+
+    case "revenue":
+      sortOption.revenue = "desc";
+      sortOption.totalInvestment = "asc";
+      sortOption.employees = "asc";
+      break;
+
+    default:
+      sortOption.employees = "desc";
+      sortOption.totalInvestment = "asc";
+      sortOption.revenue = "asc";
+  }
+
+  return sortOption;
+}
+
+companiesRouter.get("/", async (req, res) => {
+  const sort = req.query.sort;
+  const sortOption = getSortOption(sort);
+
+  const companyList = await companyModel.find().sort(sortOption);
+
+  res.send(companyList);
+});
+
+// 기업 상세 조회 APT
 companiesRouter.get(
   "/:id",
   asyncHandler(async (req, res) => {
