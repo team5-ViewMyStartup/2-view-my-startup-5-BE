0a7c7e13bd35263e43db93bb1b03259bcab04df1
ciin1411 <ciin1411@naver.com>
2024-10-03 23:47:30 +0900

    fix: second fixed compare api

--- src/apis/compare.js
+++ src/apis/compare.js
@@ -4,35 +4,6 @@ import { asyncHandler } from "../../utils/async-handler.js";
 
 export const compareRouter = express.Router();
 
-// 기업 선택 시 selectMyCount, selectOtherCount 업데이트 API
-// compareRouter.post(
-//   "/select",
-//   asyncHandler(async (req, res) => {
-//     const { companyId, isMyCompany } = req.body;
-//     const company = await Company.findOne({ id: companyId });
-
-//     if (!company) {
-//       throw new NotFoundError("회사를 찾을 수 없습니다.");
-//     }
-
-//     const query = {
-//       $inc: {},
-//     };
-
-//     if (isMyCompany) {
-//       query.$inc.selectMyCount = 1;
-//     } else {
-//       query.$inc.selectOtherCount = 1;
-//     }
-
-//     // atomic
-
-//     const updatedCompany = await Company.findOneAndUpdate({ id: companyId }, query, { new: true });
-
-//     return res.json(updatedCompany);
-//   }),
-// );
-
 /**
  * 기업명, 기업 소개, 카테고리, 누적 투자 금액, 매출액, 고용 인원이 조회됩니다.
  * 누적 투자 금액 (totalInvestment), 매출액 (revenue), 고용 인원(employees) 별 오름차순/내림차순 정렬을 제공합니다.
@@ -45,18 +16,6 @@ compareRouter.get(
   "/select",
   asyncHandler(async (req, res) => {
     const { baseCompanyId, compareCompanyId, sortKeys } = req.query;
-
-    /**
-     * compareCompanyId/sortKeys count === 1 -> string
-     * compareCompanyId/sortKeys count > 1 -> array
-     *
-     * compareCompanyId = 'asfdsdf'
-     * compareCompanyId = ['asfdsdf', 'asdfasdf']
-     *
-     * ['asfdsdf'] or [['asfdsdf', 'asdfasdf']]
-     * flat()
-     * ['asfdsdf'] or ['asfdsdf', 'asdfasdf']
-     */
 
     const compareCompanyIdList = [compareCompanyId].flat();
     const sortKeyList = [sortKeys].flat();
