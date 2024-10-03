2b47f75920e59c0e7583b26b15f698950bdb4bcb
unknown <didrkgus0426@gmail.com>
2024-10-03 22:04:24 +0900

    feat: invest in detail page to update comment and delete

--- src/apis/investments.js
+++ src/apis/investments.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { NotFoundError } from "../error.js";
 import Investment from "../models/investment.schema.js";
+import Company from "../models/company.schema.js";
+import User from "../models/user.schema.js";
 import { asyncHandler } from "../../utils/async-handler.js";
 
 export const investmentsRouter = express.Router();
@@ -21,6 +23,54 @@ investmentsRouter.get(
       throw new NotFoundError("기업에 투자한 정보가 없습니다.");
     }
     res.json(investments);
+  }),
+);
+
+investmentsRouter.put(
+  "/:investmentId",
+  asyncHandler(async (req, res) => {
+    const { investmentId } = req.params;
+    const { comment, password, nickname } = req.body;
+
+    //임시
+    const user = await User.findOne({ nickname });
+    if (!user || user.password !== password) {
+      throw new Error("잘못된 비밀번호입니다.");
+    }
+
+    const investment = await Investment.findById(investmentId);
+    if (!investment) {
+      throw new NotFoundError("투자 정보를 찾을 수 없습니다.");
+    }
+
+    investment.comment = comment;
+
+    await investment.save();
+
+    res.json(investment);
+  }),
+);
+
+investmentsRouter.delete(
+  "/:investmentId",
+  asyncHandler(async (req, res) => {
+    const { investmentId } = req.params;
+    const { password, nickname } = req.body;
+
+    // 임시
+    const user = await User.findOne({ nickname });
+    if (!user || user.password !== password) {
+      throw new Error("투자 정보를 찾을 수 없습니다.");
+    }
+
+    const investment = await Investment.findById(investmentId);
+    if (!investment) {
+      throw new NotFoundError("투자 정보를 찾을 수 없습니다.");
+    }
+
+    await investment.deleteOne();
+
+    res.json();
   }),
 );
 
