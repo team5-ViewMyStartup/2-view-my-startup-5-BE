01aa2202c43a20063b4f8d80ec3da584f0e41926
woonie_potato <backendwoonie@gmail.com>
2024-10-14 13:08:02 +0900

    refactor: refactor login-checker.js

--- src/middlewares/login-checker.js
+++ src/middlewares/login-checker.js
@@ -6,26 +6,26 @@ export const loginChecker = async (req, res, next) => {
   const bearerToken = req.get("Authorization");
 
   if (!bearerToken) {
-    throw new UnauthorizedError("API 요청에서 Authorization 헤더가 전달되지 않았습니다.");
+    return next(new UnauthorizedError("API 요청에서 Authorization 헤더가 전달되지 않았습니다."));
   }
 
   const [_, token] = (bearerToken || "").split(" ");
 
   if (!token) {
-    throw new UnauthorizedError("잘못된 토큰 형식입니다.");
+    return next(new UnauthorizedError("잘못된 토큰 형식입니다."));
   }
 
   try {
     const { email, nickname } = jwt.getPayload(token);
     const user = await User.findOne({ email }).exec();
     if (!user) {
-      throw new NotFoundError("사용자를 찾을 수 없습니다.");
+      return next(new NotFoundError("사용자를 찾을 수 없습니다."));
     }
 
     req.user = { email, nickname };
 
     next();
   } catch (err) {
-    next(UnauthorizedError(`유효하지 않은 토큰입니다. - ${err.message}`));
+    next(new UnauthorizedError(`유효하지 않은 토큰입니다. - ${err.message}`));
   }
 };
